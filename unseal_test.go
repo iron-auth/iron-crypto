31ba16878d26bbca3524e4ccc75e3b1e3b48658d
James <james@eli.cx>
2023-02-10 02:34:22 +0000

    test: unseal

---
+++ unseal_test.go
@@ -0,0 +1,102 @@
+package iron_test
+
+import (
+	"testing"
+
+	"github.com/iron-auth/iron-crypto"
+	"github.com/iron-auth/iron-crypto/ironerrors"
+	"github.com/iron-auth/iron-crypto/pw"
+	a "github.com/james-elicx/go-utils/assert"
+)
+
+func TestUnsealFailsWithNoPassword(t *testing.T) {
+	t.Parallel()
+
+	_, err := iron.Unseal[string](SealedFromNode, pw.UnsealRaw{
+		Password: pw.Password{
+			String: "",
+		},
+	}, iron.SealConfig{
+		Encryption:          SealEncryption,
+		Integrity:           SealIntegrity,
+		TTL:                 0,
+		TimestampSkewSec:    0,
+		LocalTimeOffsetMsec: 0,
+	})
+
+	a.Equals(t, err, ironerrors.ErrPasswordRequired)
+}
+
+func TestUnsealFailsWithInvalidSeal(t *testing.T) {
+	t.Parallel()
+
+	_, err := iron.Unseal[string]("", pw.UnsealRaw{
+		Password: pw.Password{
+			String: DecryptedPassword,
+		},
+	}, iron.SealConfig{
+		Encryption:          SealEncryption,
+		Integrity:           SealIntegrity,
+		TTL:                 0,
+		TimestampSkewSec:    0,
+		LocalTimeOffsetMsec: 0,
+	})
+
+	a.Equals(t, err, ironerrors.ErrBadSealHmac)
+}
+
+func TestUnsealFailsWithInvalidJson(t *testing.T) {
+	t.Parallel()
+
+	_, err := iron.Unseal[string](InvalidJsonSealedFromGo, pw.UnsealRaw{
+		Password: pw.Password{
+			String: DecryptedPassword,
+		},
+	}, iron.SealConfig{
+		Encryption:          SealEncryption,
+		Integrity:           SealIntegrity,
+		TTL:                 0,
+		TimestampSkewSec:    0,
+		LocalTimeOffsetMsec: 0,
+	})
+
+	a.Equals(t, err, ironerrors.ErrUnmarshallingObject)
+}
+
+func TestUnsealWorksWithValidJson(t *testing.T) {
+	t.Parallel()
+
+	obj, err := iron.Unseal[string](ValidJsonSealedFromGo, pw.UnsealRaw{
+		Password: pw.Password{
+			String: DecryptedPassword,
+		},
+	}, iron.SealConfig{
+		Encryption:          SealEncryption,
+		Integrity:           SealIntegrity,
+		TTL:                 0,
+		TimestampSkewSec:    0,
+		LocalTimeOffsetMsec: 0,
+	})
+
+	a.Equals(t, err, nil)
+	a.Equals(t, obj, DecryptedMessage)
+}
+
+func TestUnsealWorksWithNodeJSSeal(t *testing.T) {
+	t.Parallel()
+
+	obj, err := iron.Unseal[string](SealedFromNode, pw.UnsealRaw{
+		Password: pw.Password{
+			String: DecryptedPassword,
+		},
+	}, iron.SealConfig{
+		Encryption:          SealEncryption,
+		Integrity:           SealIntegrity,
+		TTL:                 0,
+		TimestampSkewSec:    0,
+		LocalTimeOffsetMsec: 0,
+	})
+
+	a.Equals(t, err, nil)
+	a.Equals(t, obj, DecryptedMessage)
+}
